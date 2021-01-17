ad2d5d04c2ecab5dc815634e410a32773264fbbc
Oscar Montes <oscmcojc1@live.com.mx>
2021-01-16 18:51:28 -0600

    Persistent provider data implementation with Singleton pattern

--- lib/src/bloc/provider.dart
+++ lib/src/bloc/provider.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:form_validation/src/bloc/login_bloc.dart';
-// export 'package:form_validation/src/bloc/login_bloc.dart';
-class Provider extends InheritedWidget {
-  final loginBloc = LoginBloc();
 
-  Provider({Key key, Widget child})
+class Provider extends InheritedWidget {
+
+  //Para mantener los datos al hacer hotreload
+  static Provider _instance;
+
+  factory Provider({Key key, Widget child}) {
+    if(_instance == null) {
+      _instance = new Provider._internal(key: key, child: child);
+    }
+    return _instance;
+  }
+
+  Provider._internal({Key key, Widget child})
     : super(key: key, child: child);
+
+  final loginBloc = LoginBloc();
 
   @override
   bool updateShouldNotify(covariant InheritedWidget oldWidget) => true;
